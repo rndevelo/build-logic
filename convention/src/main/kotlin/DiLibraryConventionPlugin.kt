a78c25b508e2c2c190cdeeeaae16ff522e9f60f5
Renendo <rndeveloper11501@gmail.com>
2025-09-23 22:52:22 +0200

    Refactor: Remove redundant Hilt Android dependency
    The `hilt.android` dependency has been removed from `DiLibraryConventionPlugin.kt` as it was redundant. The core Hilt functionality is provided by `hilt.core`, and Android-specific Hilt features are typically handled by other plugins or directly in Android library/application modules.

--- convention/src/main/kotlin/DiLibraryConventionPlugin.kt
+++ convention/src/main/kotlin/DiLibraryConventionPlugin.kt
@@ -10,7 +10,6 @@ class DiLibraryConventionPlugin : Plugin<Project> {
 
             dependencies {
                 add("implementation", libs.findLibrary("hilt.core").get())
-                add("implementation", libs.findLibrary("hilt.android").get())
                 add("ksp", libs.findLibrary("hilt.compiler").get())
             }
         }
